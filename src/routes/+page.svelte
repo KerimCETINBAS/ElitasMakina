5763dea96854cb43df8cdc3eea11598dcbf8fa23
kerimCetinbas <a.kerim.cetinbas@yandex.com>
2022-11-08 17:42:06 +0300

    initial

---
+++ src/routes/+page.svelte
@@ -0,0 +1,38 @@
+<div>
+  <img src="/Logo.png" alt="" />
+</div>
+
+<svelte:head>
+  <meta name="description" content="Buharli utu makinalari" />
+  <title>Elitas makina</title>
+</svelte:head>
+
+<style>
+  div {
+    display: block;
+    padding: 0;
+    margin: 0;
+    width: 100%;
+    height: 100vh;
+    background-color: #605e5f;
+  }
+
+  img {
+    padding: 0;
+    margin: 0;
+    object-fit: contain;
+    width: 100%;
+    height: 100%;
+  }
+  :global(html) {
+    padding: 0;
+    margin: 0;
+  }
+
+  :global(body) {
+    height: 100%;
+    width: 100%;
+    padding: 0;
+    margin: 0;
+  }
+</style>
